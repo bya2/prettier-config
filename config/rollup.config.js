70c63a82c2774856696bd1cea683ef383207678e
bya2 <byaa1972@gmail.com>
2024-01-17 23:39:10 +0900

    chore: 사용되지 않는 코드 제거

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,13 +1,6 @@
 import terser from "@rollup/plugin-terser";
-import { dirname } from "path";
 import { cwd } from "process";
-import { fileURLToPath } from "url";
 
-// 현재 파일
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// 스크립트가 실행된 경로
 const CWD = cwd();
 const SRC = `${CWD}/src`;
 const DIST = `${CWD}/dist`;
