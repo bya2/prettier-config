32b5a9e3e4f7061c61c03845e960fe04472022c9
bya2 <byaa1972@gmail.com>
2024-03-07 23:44:45 +0900

    feat(utils): 패턴 관련 상수 추가

--- packages/utils/src/constants.ts
+++ packages/utils/src/constants.ts
@@ -1,1 +1,10 @@
 export const DEFAULT_EXTERNAL = ["*.png", "/images/*", "/assets/*"];
+
+// [dir] [name] [hash] [ext]
+export const DEFAULT_ASSET_NAMES = "assets/[name]-[hash]";
+
+// [name] [hash] [ext]
+export const DEFAULT_CHUNK_NAMES = "chunks/[name]-[hash]";
+
+// [dir] [name] [hash] [ext]
+export const DEFAULT_ENTRY_NAMES = "[dir]/[name]-[hash]";
