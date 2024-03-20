ed8782e350b5ffc5983ab1890a95b2e57cb8421f
bya2 <byaa1972@gmail.com>
2024-03-21 00:02:24 +0900

    feat(utils): 상수 파일에 대한 외부 접근 필드 및 일부 상수 추가
    - <root>/src/constants.ts 파일에 대한 export 맵 필드 추가
    - 환경 변수 및 경로에 대한 상수 추가

--- packages/utils/src/constants.ts
+++ packages/utils/src/constants.ts
@@ -18,6 +18,11 @@ export const DOT_CJS = ".cjs";
 export const DOT_MJS = ".mjs";
 export const DOT_GLOBAL_DOT_JS = ".global.js";
 
+// 환경 변수 상수
+export const PROD = "production";
+export const DEV = "development";
+export const TEST = "test";
+
 // 외부 처리 요소
 export const DEFAULT_EXTERNAL = ["*.png", "/images/*", "/assets/*"];
 
@@ -25,3 +30,8 @@ export const DEFAULT_EXTERNAL = ["*.png", "/images/*", "/assets/*"];
 export const DEFAULT_ASSET_NAMES = "assets/[name]-[hash]"; // [dir] [name] [hash] [ext]
 export const DEFAULT_CHUNK_NAMES = "chunks/[name]-[hash]"; // [name] [hash] [ext]
 export const DEFAULT_ENTRY_NAMES = "[dir]/[name]-[hash]"; // // [dir] [name] [hash] [ext]
+
+// 경로
+export const DEFAULT_OUTBASE = "src";
+export const DEFAULT_OUTDIR = "dist";
+export const DEFAULT_ENTRY = "src/index.ts";
